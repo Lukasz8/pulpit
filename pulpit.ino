5c0c97e3594d40c9cff250427395dfba8d4063d7
SQ9MDD <rysieklabus@gmail.com>
2015-11-20 14:03:42 +0100

    sprawdzanie inicjalizacji radia, komentarze.

--- pulpit.ino
+++ pulpit.ino
@@ -20,24 +20,26 @@ RF22 rf22;
 //zmienne
 int TX = 73;
 
-
-//startup sequence
-void setup(){
-  Serial.begin(9600);
-  rf22.init(); 
-  rf22.setFrequency(434.0);
-  rf22.setTxPower(RF22_TXPOW_17DBM);  
-  
-  Serial.print("startup  ");
-}
-
 //funkcje
 void send_data(){
   //TX
   rf22.send((uint8_t*)&TX, sizeof(TX));
-  rf22.waitPacketSent();
+  //rf22.waitPacketSent();
   //tu sie program zatrzymuje - zatem nic nie wysyla
+  //moim zdaniem czeka na potwierdzenie z odbiornika jakieś ACK czy coś.
   Serial.print("funkcje  ");
+}
+
+//startup sequence
+void setup(){
+  Serial.begin(9600);
+  if (!rf22.init()){
+    Serial.println("RF22 init failed"); //sprawdzamy czy inicjowanie radia zadziałało
+  }
+  rf22.setFrequency(434.0);
+  rf22.setTxPower(RF22_TXPOW_17DBM);  
+  
+  Serial.print("startup  ");
 }
 
 //infinity loop
