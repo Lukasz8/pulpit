ac65781388cb5607c13cde4771efd155954f6100
SQ9MDD <rysieklabus@gmail.com>
2015-11-26 14:02:50 +0100

    26.11
    dodanie wysylki jednej komendy, propozycja protokołu

--- pulpit.ino
+++ pulpit.ino
@@ -2,7 +2,14 @@
 SP8XCE & SQ9MDD
 PULPIT 
 
+DATA FRAME FORMAT
+  // format ramki: XXYYZ
+  // XX - adres docelowy odbiornika zakres 10-31
+  // YY - adres wyjścia na odbiorniku zakres 10-99 10-wyjscie wirtualne czy żyjesz 11-pierwsze wyjście fizyczne
+  // Z  - komenda 1 - ON 0 - OFF
+
 CHANGELOG
+2015.11.26 - format ramki danych sprawdzenie poprawności odbioru danych z remote'a
 2015.11.24 - poprawki i synchronizacja kodu
 */
 //wysyłka danych oraz potwierdzenie odbioru
@@ -17,34 +24,33 @@ CHANGELOG
 RF22 rf22;
 
 //zmienne
-int TX = 7399;
-int RX;
 
 //funkcje
-void send_data(){
+void send_data(int TX){
+  int RX;
   //TX
   rf22.send((uint8_t*)&TX, sizeof(TX));
   rf22.waitPacketSent();
   //RX
   uint8_t buf[RF22_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
-  if (rf22.waitAvailableTimeout(500))
-  { 
-  if (rf22.recv(buf, &len))
-      {
+  if (rf22.waitAvailableTimeout(500)){ 
+    if (rf22.recv(buf, &len)){
       RX = (int&)buf;
-   Serial.print("mamy odpowiedz: ");
-   Serial.print(RX);
+      if(RX == TX){
+        Serial.println("ACK");
+      }else{
+        Serial.println("REJ");
       }
-      else
-      {
-        Serial.println("blad obierania");
-      }
-   }
-    else
-    {
-     Serial.println("brak sygnalow");
+      Serial.println(RX);      
     }
+    else{
+        Serial.println("blad obierania");
+    }
+   }
+   else{
+     Serial.println("brak sygnalow");
+   }
 }
 
 //startup sequence
@@ -61,7 +67,7 @@ void setup(){
 //infinity loop
 void loop(){
   //tutaj wysylka danych przez radio
-  send_data();
+  send_data(11101); //wysyłam dane patrz format ramki w komentarzu na górze
   Serial.print(" infinity ");
   //ulatwia synchronizowanie odbiornika po zaniku sygnalu
   delay(100); 
